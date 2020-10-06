94ebb1669e0ce0f8e6490b3153d9a7347c97cd90
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-06 10:22:40 +0300

    vince: Disable configstore usage
    * it's finally deprecated in R
    * ship disable_configstore because we have API level less than 29
    
    [Edit: Clean configstore leftover]
    
    Change-Id: Ida4ae417bb0d5a22ea7eb9500e687a656be6a0ce
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -180,9 +180,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
-# Configstore
+# Disable Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.1-service
+    disable_configstore
 
 # Display
 PRODUCT_PACKAGES += \
